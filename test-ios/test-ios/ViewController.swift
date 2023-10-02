da2c6e325ac772b7cc6301077f74f70fa7ed1811
Natasha-rare <natasha_ea5@mail.ru>
2023-10-02 20:30:59 +0300

    first commit with initialized swift project

---
+++ test-ios/test-ios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test-ios
+//
+//  Created by Наталья Автухович on 02.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
